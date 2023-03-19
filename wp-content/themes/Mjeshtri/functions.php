3caeeed1a0cca988a7025a5e66687d8e1b1dd985
ardialiqkaj <ardi.aliqkaj@gmail.com>
2023-03-19 23:49:58 +0100

    initial theme files

---
+++ wp-content/themes/Mjeshtri/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function style_enqueue()
+{
+	wp_enqueue_style('style', get_template_directory_uri() . 'style.css');
+	wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', [], null, true);
+}
+add_action('wp_enqueue_scripts', 'style_enqueue');
+
+?>
---
+++ wp-content/themes/Mjeshtri/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<h1>Mjeshtri</h1>
+
+
+<?php get_footer(); ?>
