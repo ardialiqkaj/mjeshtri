522bab79ca8cfc3d09bf571199101b36010c1a13
ardialiqkaj <ardi.aliqkaj@gmail.com>
2023-03-27 23:04:44 +0200

    initial header style, removed plugins

--- wp-content/themes/Mjeshtri/functions.php
+++ wp-content/themes/Mjeshtri/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function style_enqueue() {
-	wp_enqueue_style( 'style-name', get_template_directory_uri() . '/public/css/tailwind.css');
+	wp_enqueue_style( 'style-name', get_template_directory_uri() . './public/css/tailwind.css');
 }
 add_action( 'wp_enqueue_scripts', 'style_enqueue' );
 
--- wp-content/themes/Mjeshtri/header.php
+++ wp-content/themes/Mjeshtri/header.php
@@ -9,3 +9,13 @@
 </head>
 
 <body>
+    <header class="flex justify-between bg-[#9e2a2b] text-white h-12 items-center">
+        <img class="px-4" src="./public/images/mjeshtri-red.png" alt="logo">
+        <nav>
+            <ul class="flex">
+                <li class="px-4">Home</li>
+                <li class="px-4">About Us</li>
+                <li class="px-4">Contact</li>
+            </ul>
+        </nav>
+    </header>
