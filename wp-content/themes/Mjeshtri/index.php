4380db4a971075565a38bdc11fbf30c6614e10db
ardialiqkaj <ardi.aliqkaj@gmail.com>
2023-03-28 23:59:14 +0200

    post loop added

--- wp-content/themes/Mjeshtri/index.php
+++ wp-content/themes/Mjeshtri/index.php
@@ -1,5 +1,22 @@
 <?php get_header(); ?>
 <h1 class="">Mjeshtri</h1>
 
-
+<?php 
+	
+	if( have_posts() ):
+		
+		while( have_posts() ): the_post(); ?>
+		
+			<h3><?php the_title(); ?></h3>
+			<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+			
+			<p><?php the_content(); ?></p>
+			
+			<hr>
+		
+		<?php endwhile;
+		
+	endif;
+			
+	?>
 <?php get_footer(); ?>
